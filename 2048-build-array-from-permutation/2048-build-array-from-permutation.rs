b65250f0508c1cdb781b7d571c8af5769e1e2dc6
ASHUTOSH MISHRA <aashutoshm77@gmail.com>
2025-02-18 12:05:48 +0530

    Time: 0 ms (100%), Space: 2.4 MB (54.84%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.rs
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.rs
@@ -1,10 +1,14 @@
 impl Solution {
-    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len();
-        let mut ans = Vec::with_capacity(n);
-        for i in 0..n {
-            ans.push(nums[nums[i] as usize]);
+    pub fn build_array(mut nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len() as i32;
+        for i in 0..nums.len() {
+            let original_value = nums[nums[i] as usize];
+            let new_val = original_value % n;
+            nums[i] += new_val * n;
         }
-        ans
+        for i in 0..nums.len() {
+            nums[i] /= n; 
+        }
+        nums 
     }
 }
