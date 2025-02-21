32da4a6491ad38bd4d8e04dad5ad04e437650dad
ASHUTOSH MISHRA <aashutoshm77@gmail.com>
2025-02-21 17:59:47 +0530

    Time: 0 ms (100%), Space: 2.2 MB (64%) - LeetHub

--- 1137-height-checker/1137-height-checker.rs
+++ 1137-height-checker/1137-height-checker.rs
@@ -1,10 +1,22 @@
 impl Solution {
     pub fn height_checker(heights: Vec<i32>) -> i32 {
-        let mut expected = heights.clone();
-        expected.sort();
-        heights.iter()
-            .zip(expected.iter())
-            .filter(|(a, b)| a != b)
-            .count() as i32
+        let mut count = [0; 101];
+        for &height in &heights {
+            count[height as usize] += 1;
+        }
+        
+        let mut result = 0;
+        let mut index = 0;
+        
+        for height in 1..=100 {
+            while count[height] > 0 {
+                if heights[index] != height as i32 {
+                    result += 1;
+                }
+                index += 1;
+                count[height] -= 1;
+            }
+        }
+        result
     }
 }
