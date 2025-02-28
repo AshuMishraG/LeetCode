8f7fd4dfed082d1291aed99c34444c3633566ea4
ASHUTOSH MISHRA <aashutoshm77@gmail.com>
2025-03-01 04:37:35 +0530

    Time: 3 ms (88.57%), Space: 2.5 MB (95.71%) - LeetHub

--- 0561-array-partition/0561-array-partition.rs
+++ 0561-array-partition/0561-array-partition.rs
@@ -1,6 +1,27 @@
 impl Solution {
-    pub fn array_pair_sum(mut nums: Vec<i32>) -> i32 {
-        nums.sort_unstable();
-        nums.iter().step_by(2).sum()
+    pub fn array_pair_sum(nums: Vec<i32>) -> i32 {
+        const OFFSET: usize = 10000;
+        const SIZE: usize = 20001;
+        
+        let mut count = vec![0; SIZE];
+        
+        for num in nums {
+            count[(num + OFFSET as i32) as usize] += 1;
+        }
+        
+        let mut result = 0;
+        let mut take = true;
+        
+        for i in 0..SIZE {
+            while count[i] > 0 {
+                if take {
+                    result += i as i32 - OFFSET as i32;
+                }
+                take = !take;
+                count[i] -= 1;
+            }
+        }
+        
+        result
     }
 }
