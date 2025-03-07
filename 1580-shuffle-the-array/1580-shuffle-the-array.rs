bb3fbacf9dd8ad14beab0fecacc1d4a4c9fa68e7
ASHUTOSH MISHRA <aashutoshm77@gmail.com>
2025-03-07 23:48:59 +0530

    Time: 1 ms (71.67%), Space: 2.3 MB (96.67%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.rs
+++ 1580-shuffle-the-array/1580-shuffle-the-array.rs
@@ -1,19 +1,10 @@
 impl Solution {
-    pub fn shuffle(mut nums: Vec<i32>, n: i32) -> Vec<i32> {
-        let n = n as usize;
-        let base = 1001;
-        for i in 0..(2 * n) {
-            let new_val = if i % 2 == 0 {
-                nums[i / 2] % base
-            } else {
-                nums[n + i / 2] % base
-            };
-            nums[i] += new_val * base;
-        }
-        for i in 0..(2 * n) {
-            nums[i] /= base;
-        }
-        
-        nums
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+      let mut rslt: Vec<i32> = Vec::with_capacity(nums.len());
+      for i in 0..n {
+        rslt.push(nums[i as usize]);
+        rslt.push(nums[i as usize + n as usize]);
+      }
+      rslt
     }
 }
