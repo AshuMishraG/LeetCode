a12748d3faa4a66d38c5ba466b61db411a22a130
ASHUTOSH MISHRA <aashutoshm77@gmail.com>
2025-03-25 16:51:07 +0530

    Time: 0 ms (100%), Space: 4.1 MB (53.33%) - LeetHub

--- 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.rs
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.rs
@@ -1,13 +1,18 @@
 impl Solution {
     pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
         let index = match rule_key.as_str() {
-            "type"  => 0,
+            "type" => 0,
             "color" => 1,
-            "name"  => 2,
-            _       => unreachable!(),
+            "name" => 2,
+            _ => unreachable!(),
         };
-        items.into_iter()
-            .filter(|item| item[index] == rule_value)
-            .count() as i32
+        let rule_value = rule_value.as_str();
+        let mut count = 0;
+        for item in items.iter() {
+            if item[index].as_str() == rule_value {
+                count += 1;
+            }
+        }
+        count
     }
 }
