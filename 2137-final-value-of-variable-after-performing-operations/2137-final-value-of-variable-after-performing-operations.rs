92f43ef34b1fe48cb7306a11b581e55e173b4c7a
ASHUTOSH MISHRA <aashutoshm77@gmail.com>
2025-04-01 11:20:25 +0530

    Time: 0 ms (100%), Space: 2.1 MB (100%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.rs
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.rs
@@ -1,5 +1,7 @@
 impl Solution {
     pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
-        operations.iter().fold(0, |acc, op| acc + if op.contains('+') { 1 } else { -1 })
+        operations.iter().fold(0, |acc, op| {
+            acc + if op.chars().nth(1).unwrap() == '+' { 1 } else { -1 }
+        })
     }
 }
