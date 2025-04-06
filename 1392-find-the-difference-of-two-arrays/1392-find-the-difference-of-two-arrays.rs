d4f7430fe1c6472db87c048e399389c77cbbf1e8
ASHUTOSH MISHRA <aashutoshm77@gmail.com>
2025-04-06 17:13:18 +0530

    Time: 2 ms (57.84%), Space: 2.3 MB (41.18%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.rs
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.rs
@@ -1,12 +1,28 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn find_difference(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<Vec<i32>> {
-        let set1: HashSet<i32> = nums1.into_iter().collect();
-        let set2: HashSet<i32> = nums2.into_iter().collect();
+        const OFFSET: i32 = 1000;
+        const SIZE: usize = 2001;
+
+        let mut present1 = [false; SIZE];
+        let mut present2 = [false; SIZE];
         
-        let diff1: Vec<i32> = set1.difference(&set2).cloned().collect();
-        let diff2: Vec<i32> = set2.difference(&set1).cloned().collect();
+        for num in nums1 {
+            present1[(num + OFFSET) as usize] = true;
+        }
+        for num in nums2 {
+            present2[(num + OFFSET) as usize] = true;
+        }
+        
+        let mut diff1 = Vec::new();
+        let mut diff2 = Vec::new();
+        for i in 0..SIZE {
+            if present1[i] && !present2[i] {
+                diff1.push(i as i32 - OFFSET);
+            }
+            if present2[i] && !present1[i] {
+                diff2.push(i as i32 - OFFSET);
+            }
+        }
         
         vec![diff1, diff2]
     }
