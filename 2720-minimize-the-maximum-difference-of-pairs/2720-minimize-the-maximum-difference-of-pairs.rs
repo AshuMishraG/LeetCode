96a002b0541e02f2d35157675db3d18d73602a88
Ashutosh Mishra <aashutoshm77@gmail.com>
2025-06-13 10:17:30 +0530

    Time: 7 ms (10%), Space: 3.8 MB (30%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.rs
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.rs
@@ -1,30 +1,33 @@
 impl Solution {
-    pub fn minimize_max(nums: Vec<i32>, p: i32) -> i32 {
-        let mut a = nums;
-        a.sort_unstable();
-        let n = a.len();
-        let mut left = 0;
-        let mut right = a[n - 1] - a[0];
-        let p = p as usize;
+    fn is_possible_to_form(nums: &Vec<i32>, mut p: i32, diff: i32) -> bool {
+        let mut i = 0;
+
+        while i < nums.len() - 1 && p > 0 {
+            if nums[i + 1] - nums[i] <= diff {
+                p -= 1;
+                i += 2;
+            } else {
+                i += 1
+            }
+        }
+
+        p <= 0
+    }
+
+    pub fn minimize_max(mut nums: Vec<i32>, p: i32) -> i32 {
+        nums.sort();
+
+        let (mut left, mut right) = (0, nums[nums.len() - 1] - nums[0]);
 
         while left < right {
             let mid = left + (right - left) / 2;
-            let mut cnt = 0;
-            let mut i = 1;
-            while i < n {
-                if a[i] - a[i - 1] <= mid {
-                    cnt += 1;
-                    i += 2;
-                } else {
-                    i += 1;
-                }
-            }
-            if cnt >= p {
+            if Self::is_possible_to_form(&nums, p, mid) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
+
         left
     }
 }
